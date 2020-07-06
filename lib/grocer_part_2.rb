f4e7f9f62a2d1731233c92e4fc06817c417870c0
Brian Lego <legobw@gmail.com>
2020-07-06 23:59:21 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -24,10 +24,10 @@ def apply_coupons(cart, coupons)
     end
   end
   new_cart.map do |new_cart_item|
-    
+    binding.pry
     if new_cart_item[:count] == 0
       new_cart.delete(new_cart_item)
-      binding.pry
+      
     end  
   if coupons == []
     cart
