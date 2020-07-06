582b954ba0908eb280281e41f9a9a34f1231ed53
Brian Lego <legobw@gmail.com>
2020-07-06 23:31:43 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -1,25 +1,60 @@
 require_relative './part_1_solution.rb'
+require 'pry'
 
 def apply_coupons(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  coupon_names = coupons.each_with_object([]) {|names, a| a << names[:item]}
+  #binding.pry
+  new_cart = []
+  cart.each do |cart_item|
+    coupons.each do |item|
+      #binding.pry
+      if item[:item] == cart_item[:item] && cart_item[:count] >= item[:num]
+      item[:clearance] = cart_item[:clearance]
+      item[:item] = "#{item[:item]} W/COUPON"
+      item[:count] = ((cart_item[:count]/item[:num]).to_int) * item[:num]
+      cart_item[:count] = cart_item[:count]%item[:num]
+      item.delete(:num)
+      item[:price] = item[:cost]/item[:count]
+      item.delete(:cost)
+      new_cart.push(item)
+      new_cart.push(cart_item)
+      #binding.pry
+      end
+    end
+  end
+  if coupons == []
+    cart
+  else
+    new_cart
+  end 
+  #binding.pry
 end
 
 def apply_clearance(cart)
-  # Consult README for inputs and outputs
-  #
-  # REMEMBER: This method **should** update cart
+  new_cart = []
+  cart.each do |item|
+    if item[:clearance] == false 
+      new_cart.push(item)
+    elsif item[:clearance] == true 
+      item[:price] = (item[:price]-(item[:price]*0.2)).round(2)
+      new_cart.push(item)
+    end
+  end  
+  new_cart
 end
 
+
+
 def checkout(cart, coupons)
-  # Consult README for inputs and outputs
-  #
-  # This method should call
-  # * consolidate_cart
-  # * apply_coupons
-  # * apply_clearance
-  #
-  # BEFORE it begins the work of calculating the total (or else you might have
-  # some irritated customers
+  #binding.pry
+  new_cart = apply_clearance(apply_coupons(consolidate_cart(cart), coupons))
+  binding.pry
+  final_total = 0 
+  new_cart.map do |item|
+    
+    final_total += item[:price]*item[:count]
+    #binding.pry
+  end  
+  final_total
+  #binding.pry
 end
